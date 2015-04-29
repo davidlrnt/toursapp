2f7ae4c6d2d02ecff82358802e2e04a1ed47eadb
peterhwang2010 <inhohwang2010@gmail.com>
2015-04-29 17:11:52 -0400

    fixed small bugs in friends logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ActiveRecord::Base
 
     has_many :active_friends, -> { where(friendships: { approved: true}) }, :through => :friendships, :source => :friend
     has_many :passive_friends, -> { where(friendships: { approved: true}) }, :through => :passive_friendships, :source => :user
-    has_many :pending_friends, -> { where(friendships: { approved: true}) }, :through => :friendships, :source => :friend
+    has_many :pending_friends, -> { where(friendships: { approved: false}) }, :through => :friendships, :source => :friend
     has_many :requested_friendships, -> { where(friendships: { approved: false}) }, :through => :passive_friendships, :source => :user
 
 
@@ -57,10 +57,10 @@ class User < ActiveRecord::Base
 
   acts_as_messageable
 
-
   def friends
-      active_friends | passive_friends
+    active_friends | passive_friends
   end
+
 
   def mailboxer_name
    self.name
