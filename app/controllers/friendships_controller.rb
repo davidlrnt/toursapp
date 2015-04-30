690485f11ec056608af79a818a494a72beabb711
mandelbulb <mandelbulb@github.com>
2015-04-30 15:44:00 -0400

    fixed user friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,10 +4,6 @@ class FriendshipsController < ApplicationController
     def create
       @friendship = current_user.friendships.build(:friend_id => params[:friend_id], approved: "false")
       if @friendship.save
-        flash[:notice] = "Friend requested."
-        redirect_to :back
-      else
-        flash[:error] = "Unable to request friendship."
         redirect_to :back
       end
     end
