34abd4ac66c6015c6e00f19ec8a3ca8824db69f7
legendss <david.l.castilla@gmail.com>
2015-05-16 19:38:46 -0400

    update

--- app/models/ImageUploader.rb
+++ app/models/ImageUploader.rb
@@ -1,15 +1,15 @@
-class ImageUploader
-  storage :fog
+# class ImageUploader
+#   storage :fog
 
-  def store_dir
-    "uploads/#{image.class.to_s.underscore}/#{mounted_as}/#{image.id}"
-  end
+#   def store_dir
+#     "uploads/#{image.class.to_s.underscore}/#{mounted_as}/#{image.id}"
+#   end
 
-  def cache_dir
-    "/tmp/image-cache"
-  end
+#   def cache_dir
+#     "/tmp/image-cache"
+#   end
 
-  def extension_white_list
-    %w(jpg jpeg png)
-  end
-end
+#   def extension_white_list
+#     %w(jpg jpeg png)
+#   end
+# end
